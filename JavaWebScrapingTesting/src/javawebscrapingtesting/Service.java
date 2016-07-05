4a29ab6dc61c3093d82a96d61bea3c3ad1efc221
Graham Pye <frahamp@gmail.com>
2016-07-05 20:55:32 +0100

    Class for processing each service

---
+++ JavaWebScrapingTesting/src/javawebscrapingtesting/Service.java
@@ -0,0 +1,18 @@
+package javawebscrapingtesting;
+
+/**
+ *
+ * @author Graham
+ */
+public class Service {
+    private String ind;
+    private String plannedArrival;
+    private String actualArrival;
+    private String origin;
+    private String platform;
+    private String id;
+    private String trainOperatorID;
+    private String Desination;
+    private String plannedDeparture;
+    private String actualDeparture;
+}
