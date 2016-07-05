2feba6e016278ffb85bc3e944dee0c58c0e2c1f6
Graham Pye <frahamp@gmail.com>
2016-07-05 16:00:38 +0100

    Added testing

--- JavaWebScrapingTesting/src/javawebscrapingtesting/JavaWebScrapingTesting.java
+++ JavaWebScrapingTesting/src/javawebscrapingtesting/JavaWebScrapingTesting.java
@@ -1,8 +1,6 @@
 package javawebscrapingtesting;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
